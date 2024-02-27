af5d0b70ffe38ecd5ae338a8edd616c237131a92
samanft <poopeekaska@gmail.com>
2024-02-27 11:09:27 +0100

    added search icon

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,26 +1,39 @@
 <template>
-    <input
-      type="search"
-      v-model="searchQuery"
-      class="tertiary-1 secondary empty-state-message"
-      placeholder="Search for a house"
-    />
-  </template>
-  
-  <script>
-  import { ref, watchEffect } from 'vue';
-  
-  export default {
-    setup(props, { emit }) {
-      const searchQuery = ref('');
-  
-      watchEffect(() => {
-        emit('updateSearchQuery', searchQuery.value);
-      });
-  
-      return {
-        searchQuery,
-      };
-    },
-  };
-  </script>
+  <input
+    type="search"
+    v-model="searchQuery"
+    class="tertiary-1 secondary empty-state-message"
+    placeholder="Search for a house"
+    :style="{ backgroundImage: `url(${searchIcon})` }"
+  />
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+import searchIcon from '../assets/ic_search@3x.png';
+
+export default {
+  setup(props, { emit }) {
+    const searchQuery = ref('');
+
+    watchEffect(() => {
+      emit('updateSearchQuery', searchQuery.value);
+    });
+
+    return {
+      searchQuery,
+      searchIcon,
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+input {
+  background-repeat: no-repeat;
+  background-position: 15px center; /* Adjust the first value to move the image further from the left edge */
+  background-size: 20px;
+  text-indent: 30px; /* Add indentation to the text */
+}
+</style>
