e35f2bb3b10a1af7cbcdb51f0c01e02fda4513d6
samanft <poopeekaska@gmail.com>
2024-02-27 12:18:59 +0100

    added icon for add button

--- src/components/Houses.vue
+++ src/components/Houses.vue
@@ -4,6 +4,7 @@
       <div class="flex-1">
         <p class="header-1">Houses</p>
         <button class="button primary-background buttons-and-tabs">
+          <img id="plusIcon" src="../assets/ic_plus_white@3x.png" alt="Add house" />
           Create new
         </button>
       </div>
@@ -59,9 +60,16 @@ input {
   border: none;
   padding: 10px 20px;
   border-radius: 5px;
+  display: flex;
+  align-items: center;
 }
 
 .background-1 {
   min-height: 100vh;
+}
+
+#plusIcon {
+  width: 20px;
+  margin-right: 15px;
 }
 </style>
